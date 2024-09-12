569deceac7d3e0f16a4e984c5accd19415b20eab
Moesha Keswani <keswanimoe@hotmail.co.uk>
2024-09-12 10:38:31 +0100

    Init pygame and screen

---
+++ game.py
@@ -0,0 +1,13 @@
+import pygame
+import math
+import random
+# import pygame from mixer
+
+# Initalise pygame
+pygame.init()
+
+# Create screen - using standard width and height
+res = (800, 600)
+
+screen = pygame.display.set_mode(res)
+
